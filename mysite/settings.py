798b73d426c970d260713d573de130fa262c4448
malevolentninja <malevolentninja.malevolentninja@gmail.com>
2017-12-09 12:15:47 +0000

    changed database to sqlite3

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,13 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
-        'NAME': 'django',
-        'USER': 'malevolentninja',
-        'PASSWORD': 'themat',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
